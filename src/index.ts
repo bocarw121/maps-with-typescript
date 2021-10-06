85d9995d5a16afda6b7542d511791d122155c19f
bocarw121 <bocaralhassanwane@gmail.com>
2021-10-06 10:00:02 -0400

    Add Map class and Google maps api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
 const user = new User();
 
-console.log(user);
-
 const company = new Company();
 
-console.log(company);
+new CustomMap("map");
