c304dc896bcaa842dfdc80c6c9d963369445d837
bocarw121 <bocaralhassanwane@gmail.com>
2021-10-06 10:35:12 -0400

    Add Mappable interface

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
 const user = new User();
-
 const company = new Company();
+const customMap = new CustomMap("map");
 
-new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
